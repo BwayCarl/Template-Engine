ceb492a25d2509957294ac500651d977dc1ac634
Carlos <bwaycarl@gmail.com>
2020-08-31 11:11:21 -0400

    Updated constructor.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,11 +1,9 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 
-class Intern {
-    constructor (name, role, id, email, school) {
-        this.name = name;
-        this.role = role;
-        this.id = id;
-        this.email = email;
+const Employee = require("./Employee");
+
+class Intern extends Employee{
+    constructor (school) {
         this.school = school;
     }
 }
