8d8b341852b869bd1080caa5648c32126a8f2553
Carlos <bwaycarl@gmail.com>
2020-08-31 09:57:32 -0400

    Added Engineer constructor.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Engineer {
+    constructor (name, role, id, email, github) {
+        this.name = name;
+        this.role = role;
+        this.id = id;
+        this.email = email;
+        this.github = github;
+    }
+}
+module.exports = Engineer;
