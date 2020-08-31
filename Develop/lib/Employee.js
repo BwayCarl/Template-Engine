1a0de75de58a732a0ac6b5a920245bf53a5387d5
Carlos <bwaycarl@gmail.com>
2020-08-31 15:48:57 -0400

    Update Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,9 +1,8 @@
 // TODO: Write code to define and export the Employee class
 
 class Employee {
-    constructor(name, role, id, email) {
+    constructor(name, id, email) {
         this.name = name;
-        this.role = role;
         this.id = id;
         this.email = email;
 
