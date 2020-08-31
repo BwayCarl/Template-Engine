30419806263f33721df8847ec591fa1060ca305a
Carlos <bwaycarl@gmail.com>
2020-08-31 09:56:57 -0400

    Add Manager contstructor.

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,13 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+class Manager {
+    constructor (name, role, id, email, officeNumber) {
+        this.name = name;
+        this.role = role;
+        this.id = id;
+        this.email = email;
+        this.officeNumber = officeNumber;
+    }
+}
+
+module.exports = Manager;
