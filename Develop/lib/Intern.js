c8e45ee83c073d9135c89fc2c5bde43002b3ec9a
Carlos <bwaycarl@gmail.com>
2020-08-31 09:58:14 -0400

    Added Intern constructor.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+class Intern {
+    constructor (name, role, id, email, school) {
+        this.name = name;
+        this.role = role;
+        this.id = id;
+        this.email = email;
+        this.school = school;
+    }
+}
+module.exports = Intern;
