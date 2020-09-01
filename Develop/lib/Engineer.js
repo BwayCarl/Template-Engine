893dd70a42028ca74598af5f0d8ae172b729fca0
Carlos <bwaycarl@gmail.com>
2020-09-01 07:52:23 -0400

    Add functions and super class.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,8 +2,16 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor (github) {
+    constructor (name, id, email, github) {
+        super (name, id, email);
         this.github = github;
+    }
+    getGithub () {
+        return this.github;
+    }
+
+    getRole () {
+        return "Engineer";
     }
 }
 module.exports = Engineer;
