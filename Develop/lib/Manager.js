71ac0db8899899be03b8cd25297d64ba3b65c2b5
Carlos <bwaycarl@gmail.com>
2020-09-01 11:01:22 -0400

    Update class.

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -3,6 +3,7 @@ const Employee = require("./Employee");
 
 class Manager extends Employee {
     constructor (name, id, email) {
+        super(name, id, email)
         this.officeNumber = officeNumber;
     }
 
