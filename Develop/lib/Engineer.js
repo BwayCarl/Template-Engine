0ee9d2b3de451ef6422c751eb7255e38c2f09d77
Carlos <bwaycarl@gmail.com>
2020-09-01 10:47:50 -0400

    Update class.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,7 +2,7 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor (name, id, email, github) {
+    constructor (name, id, email) {
         super (name, id, email);
         this.github = github;
     }
