1d0e773ceeb3bae114d28c0716aea22729482b1d
Carlos <bwaycarl@gmail.com>
2020-09-01 13:42:19 -0400

    Fixed class.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -3,7 +3,7 @@
 const Employee = require("./Employee");
 
 class Intern extends Employee{
-    constructor (name, id, email) {
+    constructor (name, id, email, school) {
         super (name, id, email)
         this.school = school;
     }
