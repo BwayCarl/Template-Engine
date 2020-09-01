620a36b954cbaad65bb5cf08601cd57235fd6b09
Carlos <bwaycarl@gmail.com>
2020-09-01 07:52:53 -0400

    Add functions and super class.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -3,8 +3,17 @@
 const Employee = require("./Employee");
 
 class Intern extends Employee{
-    constructor (school) {
+    constructor (name, id, email, school) {
+        super (name, id, email)
         this.school = school;
+    }
+
+    getSchool () {
+        return this.school;
+    }
+
+    getRole () {
+        return "Intern";
     }
 }
 module.exports = Intern;
