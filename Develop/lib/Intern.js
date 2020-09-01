e00d527b029c50d85d7708f9ca4e11874d460996
Carlos <bwaycarl@gmail.com>
2020-09-01 10:58:48 -0400

    Update class.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -3,7 +3,7 @@
 const Employee = require("./Employee");
 
 class Intern extends Employee{
-    constructor (name, id, email, school) {
+    constructor (name, id, email) {
         super (name, id, email)
         this.school = school;
     }
